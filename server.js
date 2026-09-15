2a30b12cd6523cbeb744b6177b7fd30a6124df7a
Miringidz23 <miringidz23@gmail.com>
2026-09-15 10:37:36 +0100

    Remove DROP TABLE command from initDatabase

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ async function initDatabase() {
   const client = await pool.connect();
   try {
     await client.query('BEGIN');
-
+     await client.query(`DROP TABLE IF EXISTS order_history, order_items, orders, products, wilayas, users, settings CASCADE;`);
     // -------- جدول المستخدمين --------
     await client.query(`
       CREATE TABLE IF NOT EXISTS users (
